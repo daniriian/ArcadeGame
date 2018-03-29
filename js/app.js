519bf7fd287013a8de9f63dd5152cabcbbd4e0be
DRT <daniriian@gmail.com>
2018-03-30 00:25:37 +0300

    add gem on canvas

--- js/app.js
+++ js/app.js
@@ -46,59 +46,99 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
-var Player = function() {
-    this.sprite = "images/char-boy.png";
-    this.x = 200;
-    this.y = 400;
-};
-
-// Is called every time the player position is updated
-Player.prototype.update = function() {
-
-    // If the player reaches the water
-    if (player.y < 20) {
-        score++;
-        document.getElementById('playerScore').innerHTML = score;
-        this.reset();
+class Player {
+    constructor() {
+        this.sprite = "images/char-pink-girl.png";
+        this.x = 200;
+        this.y = 400;
     }
-};
 
-// Draw the player on the screen, required method for game
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    // Is called every time the player position is updated
+    update() {
+        // If the player reaches the water
+        if (player.y < 20) {
+            score++;
+            document.getElementById('playerScore').innerHTML = score;
+            this.reset();
+        }
+    }
 
-//Handle the player movement in all directions
-Player.prototype.handleInput = function(direction) {
-    if (direction == "left" && this.x > 0) {
-        this.x -= 50;
+    // Draw the player on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
-    if (direction == "right" && this.x < 400) {
-        this.x += 50;
-    }
-    if (direction == "up" && this.y > 3) {
-        this.y -= 50;
-    }
-    if (direction == "down" && this.y < 400) {
-        this.y += 50;
-    }
-};
 
-// Called when the player and the enemy touch each other
-Player.prototype.reset = function() {
-    this.x = 200;
-    this.y = 320;
-};
+    //Handle the player movement in all directions
+    handleInput(direction) {
+        if (direction == "left" && this.x > 0) {
+            this.x -= 50;
+        }
+        if (direction == "right" && this.x < 400) {
+            this.x += 50;
+        }
+        if (direction == "up" && this.y > 3) {
+            this.y -= 50;
+        }
+        if (direction == "down" && this.y < 400) {
+            this.y += 50;
+        }
+    }
+
+    // Called when the player and the enemy touch each other
+    reset() {
+        this.x = 200;
+        this.y = 320;
+    }
+}
+
+class Gem {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.sprite = "images/Gem Blue.png";
+        console.log(`add gem at ${x} ${y}`);
+    }
+
+    render() {
+        "use strict";
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
+
+    update() {
+        this.checkCollision();
+        console.log('update');
+    }
+
+    checkCollision() {
+        "use strict";
+        // Set hitboxes for collision detection
+        var playerBox = { x: player.x, y: player.y, width: 50, height: 40 };
+        var gemBox = { x: this.x, y: this.y, width: 60, height: 70 };
+        // Check for collisions, if playerBox intersects gemBox, we have one
+        if (playerBox.x < gemBox.x + gemBox.width &&
+            playerBox.x + playerBox.width > gemBox.x &&
+            playerBox.y < gemBox.y + gemBox.height &&
+            playerBox.height + playerBox.y > gemBox.y) {
+            // Collision detected, call collisionDetected function
+            // this.collisionDetected();
+        }
+    }
+}
+
 // Now instantiate your objects.
 var enemy1 = new Enemy(-90, 60, 200);
 var enemy2 = new Enemy(-190, 145, 200);
-var enemy3 = new Enemy(-290, 60, 200);
+var enemy3 = new Enemy(-290, 230, 200);
+
+
 
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [enemy1, enemy2, enemy3];
 // Place the player object in a variable called player
 var player = new Player();
+
+var gem = new Gem(300, 220);
+var gem = new Gem(100, 100);
 
 
 // This listens for key presses and sends the keys to your
--- js/engine.js
+++ js/engine.js
@@ -94,6 +94,7 @@ var Engine = (function(global) {
             enemy.update(dt);
         });
         player.update();
+        gem.update();
     }
 
     /* This function initially draws the "game level", it will then call
@@ -107,19 +108,19 @@ var Engine = (function(global) {
          * for that particular row of the game level.
          */
         var rowImages = [
-                'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+                'images/water-block.png', // Top row is water
+                'images/stone-block.png', // Row 1 of 3 of stone
+                'images/stone-block.png', // Row 2 of 3 of stone
+                'images/stone-block.png', // Row 3 of 3 of stone
+                'images/grass-block.png', // Row 1 of 2 of grass
+                'images/grass-block.png' // Row 2 of 2 of grass
             ],
             numRows = 6,
             numCols = 5,
             row, col;
-        
+
         // Before drawing, clear existing canvas
-        ctx.clearRect(0,0,canvas.width,canvas.height)
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
 
         /* Loop through the number of rows and columns we've defined above
          * and, using the rowImages array, draw the correct image for that
@@ -154,6 +155,7 @@ var Engine = (function(global) {
         });
 
         player.render();
+        gem.render();
     }
 
     /* This function does nothing but it could have been a good place to
@@ -173,7 +175,10 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/Heart.png',
+        'images/char-pink-girl.png',
+        'images/Gem Blue.png'
     ]);
     Resources.onReady(init);
 
